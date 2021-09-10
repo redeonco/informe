81eacf512f7fe19e809c58f88a6d76cae920d8e7
tonycarvalho1994 <tony.carvalho1994@gmail.com>
2021-09-10 14:23:53 -0300

    Melhorias

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import home, FormulariosList, POPView, AlvarasView, filtra_formularios
+from .views import (
+    home,
+    FormulariosList,
+    POPView,
+    AlvarasView,
+    filtra_formularios,
+    filtra_pop,
+    filtra_filial
+)
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,4 +15,6 @@ urlpatterns = [
     path('pop/', POPView.as_view(), name='pop'),
     path('alvaras_licenciamento/', AlvarasView.as_view(), name='alvaras_licenciamento'),
     path('filtra_formularios/', filtra_formularios, name='filtra_formularios'),
+    path('filtra_pop/', filtra_pop, name='filtra_pop'),
+    path('filtra_filial/', filtra_filial, name='filtra_filial'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import FormularioPadrao, POP, Alvaras, Departamentos
+from .models import FormularioPadrao, POP, Alvaras, Departamentos, Filial
 
 from django.core import serializers
 from django.http import HttpResponse
@@ -13,13 +13,28 @@ def home(request):
 class FormulariosList(ListView):
     model = FormularioPadrao
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['departamentos'] = Departamentos.objects.all().order_by('departamento')
+        return context
+
 
 class POPView(ListView):
     model = POP
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['departamentos'] = Departamentos.objects.all().order_by('departamento')
+        return context
+
 
 class AlvarasView(ListView):
     model = Alvaras
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filial'] = Filial.objects.all().order_by('filial')
+        return context
 
 
 def filtra_formularios(request):
@@ -27,4 +42,20 @@ def filtra_formularios(request):
     departamento = Departamentos.objects.get(id=depart)
 
     qs_json = serializers.serialize('json', departamento.formulariopadrao_set.all())
+    return HttpResponse(qs_json, content_type='application/json')
+
+
+def filtra_pop(request):
+    depart = request.GET['outro_param']
+    departamento = Departamentos.objects.get(id=depart)
+
+    qs_json = serializers.serialize('json', departamento.pop_set.all())
+    return HttpResponse(qs_json, content_type='application/json')
+
+
+def filtra_filial(request):
+    filial_id = request.GET['outro_param']
+    filial = Filial.objects.get(id=filial_id)
+
+    qs_json = serializers.serialize('json', filial.alvaras_set.all())
     return HttpResponse(qs_json, content_type='application/json')
