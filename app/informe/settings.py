2a393151ce595d70347f8b5b90bc4e643252b573
tonycarvalho1994 <tony.carvalho1994@gmail.com>
2021-09-19 18:26:55 -0300

    Several fixes. Static Files + nginx + uWSGI + docker-compose-deploy.yml

--- app/informe/settings.py
+++ app/informe/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!(v*&sk%&tcw&rf97$4v7#!&0bju5q=rp%(($r3-e(53=g&d-5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
@@ -137,11 +143,6 @@ MEDIA_URL = '/static/media/'
 
 STATIC_ROOT = '/vol/web/static'
 MEDIA_ROOT = '/vol/web/media'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/static/',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
